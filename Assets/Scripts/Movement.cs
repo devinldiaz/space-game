0862f8cf9fb07f907ce7c2f6d1216fb8699c12e0
Devin <devinlainediaz@gmail.com>
2025-10-18 22:56:20 -0400

    player movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,18 +3,58 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
+    // create variable
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotate;
+    [SerializeField] float thrustStrength = 1000f;
+    [SerializeField] float rotationStrength = 100f;
+    Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     void OnEnable()
     {
         thrust.Enable();
+        rotate.Enable();
     }
 
-    void Update()
+    void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-            Debug.Log("Thrusting!");
+            rb.AddRelativeForce(Vector3.up * Time.fixedDeltaTime * thrustStrength);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotate.ReadValue<float>();
+
+        //transform.Rotate(-Vector3.forward * rotationInput * rotationStrength * Time.fixedDeltaTime);
+
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+    }
+
+    private void ApplyRotation(float rotationThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 }
